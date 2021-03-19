29a83705688d2226b23d731ebced196455fcab3b
AlonShirion <shirion.alon@gmail.com>
2021-03-19 14:59:25 +0200

    Switching to TypeORM

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -15,6 +15,7 @@ import {COOKIE_NAME, FORGET_PASSWORD_PREFIX} from '../constants';
 import {UsernamePasswordInput} from './UsernamePasswordInput';
 import {validateRegister} from '../utils/validateRegister';
 import argon2 from 'argon2';
+import {getConnection} from 'typeorm';
 
 @ObjectType()
 class FieldError {
@@ -40,7 +41,7 @@ export class UserResolver {
   async changePassword(
     @Arg('token') token: string,
     @Arg('newPassword') newPassword: string,
-    @Ctx() {redis, em, req}: MyContext
+    @Ctx() {redis, req}: MyContext
   ): Promise<UserResponse> {
     if (newPassword.length <= 2) {
       return {
@@ -67,7 +68,8 @@ export class UserResolver {
       };
     }
 
-    const user = await em.findOne(User, {id: parseInt(userId)});
+    const userIdNum = parseInt(userId);
+    const user = await User.findOne(userIdNum);
 
     if (!user) {
       return {
@@ -80,8 +82,10 @@ export class UserResolver {
       };
     }
 
-    user.password = await argon2.hash(newPassword);
-    await em.persistAndFlush(user);
+    await User.update(
+      {id: userIdNum},
+      {password: await argon2.hash(newPassword)}
+    );
 
     await redis.del(key);
 
@@ -92,11 +96,8 @@ export class UserResolver {
   }
 
   @Mutation(() => Boolean)
-  async forgotPassword(
-    @Arg('email') email: string,
-    @Ctx() {em, redis}: MyContext
-  ) {
-    const user = await em.findOne(User, {email});
+  async forgotPassword(@Arg('email') email: string, @Ctx() {redis}: MyContext) {
+    const user = await User.findOne({where: {email}});
 
     if (!user) {
       // the email is not in the db
@@ -121,20 +122,19 @@ export class UserResolver {
   }
 
   @Query(() => User, {nullable: true})
-  async me(@Ctx() {req, em}: MyContext) {
+  me(@Ctx() {req}: MyContext) {
     // you are not logged in
     if (!req.session.userId) {
       return null;
     }
 
-    const user = await em.findOne(User, {id: req.session.userId});
-    return user;
+    return User.findOne(req.session.userId);
   }
 
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() {em, req}: MyContext
+    @Ctx() {req}: MyContext
   ): Promise<UserResponse> {
     const errors = validateRegister(options);
 
@@ -143,14 +143,22 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-      email: options.email,
-    });
+    let user;
 
     try {
-      await em.persistAndFlush(user);
+      // The same as User.create({}).save()
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values({
+          username: options.username,
+          password: hashedPassword,
+          email: options.email,
+        })
+        .returning('*')
+        .execute();
+      user = result.raw[0];
     } catch (err) {
       // || err.detail.includes("already exists")) {
       if (err.code === '23505') {
@@ -176,13 +184,12 @@ export class UserResolver {
   async login(
     @Arg('usernameOrEmail') usernameOrEmail: string,
     @Arg('password') password: string,
-    @Ctx() {em, req}: MyContext
+    @Ctx() {req}: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(
-      User,
+    const user = await User.findOne(
       usernameOrEmail.includes('@')
-        ? {email: usernameOrEmail}
-        : {username: usernameOrEmail}
+        ? {where: {email: usernameOrEmail}}
+        : {where: {username: usernameOrEmail}}
     );
 
     if (!user) {
