1aa09d66ee0784aab6a1e3c5de96155d442a4b5f
AlonShirion <shirion.alon@gmail.com>
2021-03-27 13:16:56 +0300

    DataLoader

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from 'express';
 import {Redis} from 'ioredis';
+import {createUserLoader} from './utils/createUserLoader';
+import {createUpdootLoader} from './utils/createUpdootLoader';
 
 declare module 'express-session' {
   export interface SessionData {
@@ -11,4 +13,6 @@ export type MyContext = {
   req: Request;
   redis: Redis;
   res: Response;
+  userLoader: ReturnType<typeof createUserLoader>;
+  updootLoader: ReturnType<typeof createUpdootLoader>;
 };
